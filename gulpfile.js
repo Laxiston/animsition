23edd62f4f38ae9bd2c0a822e8cc38f632c3646d
Konstantin Karlovich <dwar.xperia@gmail.com>
2018-05-11 20:28:21 +0300

    Fixed for jQuery 3.x

--- gulpfile.js
+++ gulpfile.js
@@ -17,77 +17,77 @@ var stylish = require('jshint-stylish');
 var uglify = require('gulp-uglify');
 
 var banner = [
-'/*!',
-' * <%= pkg.name %> v<%= pkg.version %>',
-' * <%= pkg.description %>',
-' * <%= pkg.homepage %>',
-' * License : <%= pkg.license %>',
-' * Author : <%= pkg.author.name %> (<%= pkg.author.url %>)',
-' */',
-''].join('\n');
+    '/*!',
+    ' * <%= pkg.name %> v<%= pkg.version %>',
+    ' * <%= pkg.description %>',
+    ' * <%= pkg.homepage %>',
+    ' * License : <%= pkg.license %>',
+    ' * Author : <%= pkg.author.name %> (<%= pkg.author.url %>)',
+    ' */',
+    ''
+].join('\n');
 
 var dirs = {
-  src:'./src',
-  dist:'./dist',
-  sandbox:'./sandbox'
+    src: './src',
+    dist: './dist',
+    sandbox: './sandbox'
 };
 
 gulp.task('css', function () {
-  var processors = [
-    cssImport(),
-    autoprefixer({browsers: ['> 1%', 'last 2 versions']}),
-  ];
-  return gulp
-    .src(dirs.src + '/css/' + pkg.name + '.css')
-    .pipe(header(banner, {pkg:pkg}))
-    .pipe(postcss(processors))
-    .pipe(gulp.dest(dirs.dist + '/css'))
-    .pipe(cssnano({
-      autoprefixer: false,
-      reduceIdents: false,
-      mergeIdents: false
-    }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(dirs.dist + '/css'));
+    var processors = [
+        cssImport(),
+        autoprefixer({ browsers: ['> 1%', 'last 2 versions'] })
+    ];
+    return gulp
+        .src(dirs.src + '/css/' + pkg.name + '.css')
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(postcss(processors))
+        .pipe(gulp.dest(dirs.dist + '/css'))
+        .pipe(cssnano({
+            autoprefixer: false,
+            reduceIdents: false,
+            mergeIdents: false
+        }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(dirs.dist + '/css'));
 });
 
-
-gulp.task('js', function(){
-  return gulp
-    .src(dirs.src + '/js/' + pkg.name + '.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish))
-    .pipe(header(banner, { pkg:pkg }))
-    .pipe(gulp.dest(dirs.dist + '/js'))
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(header(banner, { pkg:pkg }))
-    .pipe(gulp.dest(dirs.dist + '/js'));
+gulp.task('js', function () {
+    return gulp
+        .src(dirs.src + '/js/' + pkg.name + '.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest(dirs.dist + '/js'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest(dirs.dist + '/js'));
 });
 
-gulp.task('serve', function() {
-  browserSync.init({
-    server: {
-      baseDir: '.',
-      directory: true
-    }
-  });
-  gulp.watch([dirs.src + '/css/**/*.css'], ['css']);
-  gulp.watch([dirs.src + '/js/*.js'], ['js']);
-  gulp.watch([
-    dirs.dist + '/**/*.min.{css,js}',
-    dirs.sandbox + '/*.{css,html}'
-  ]).on('change', browserReload);
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: '.',
+            directory: true
+        }
+    });
+    gulp.watch([dirs.src + '/css/**/*.css'], ['css']);
+    gulp.watch([dirs.src + '/js/*.js'], ['js']);
+    gulp.watch([
+        dirs.dist + '/**/*.min.{css,js}',
+        dirs.sandbox + '/*.{css,html}'
+    ]).on('change', browserReload);
 });
 
-gulp.task('cleanup', function(cb){
-  return del([ dirs.dist ],cb);
+gulp.task('cleanup', function (cb) {
+    return del([dirs.dist], cb);
 });
 
-gulp.task('default', ['build'], function(cb) {
-  runSequence('serve', cb);
+gulp.task('default', ['build'], function (cb) {
+    runSequence('serve', cb);
 });
 
-gulp.task('build', ['cleanup'], function(cb){
-  runSequence('js', 'css', cb);
+gulp.task('build', ['cleanup'], function (cb) {
+    runSequence('js', 'css', cb);
 });
